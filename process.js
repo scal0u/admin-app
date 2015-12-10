140b1bb85ff7e1bf3e7966238358a7c556a20a14
Pascal Conges <pascal.conges@brewers.co.uk>
2015-12-10 13:33:12 +0000

    Created "process.launch()" function

--- process.js
+++ process.js
@@ -2,6 +2,13 @@ function Process(n, t, r) {
 	this.name = n;
 	this.type = t;
 	this.requirements = r;
+
+	this.launch = function() {
+		for (x in this.requirements) {
+			if(!this.requirements[x]) console.log("Please enter your "+x+" below:");
+		};
+	};
+
 }
 
 function User(ln, fn, dob) {
@@ -16,5 +23,7 @@ function User(ln, fn, dob) {
 }
 
 
-var thisUser = new User('Conges', 'Pascal', '19871611');
-var inscriptionMaif = new Process("S'inscrire à la MAIF", "subscription", [thisUser.hasFullName(), thisUser.mobile]);
+var thisUser = new User('Conges', 'Pascal');
+var inscriptionMaif = new Process("S'inscrire à la MAIF", "subscription", {"first name":thisUser.first_name, "date of birth":thisUser.mobile});
+
+inscriptionMaif.launch();
