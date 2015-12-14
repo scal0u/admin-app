cc7fdd318321936e82852d172d0b996ae088dc17
Pascal Congès <pascal@aufondagauche.fr>
2015-12-14 22:41:42 +0000

    Encapsulated condition functions. Worked at last!

--- process.js
+++ process.js
@@ -3,7 +3,7 @@ output = "";
 // Generates a form
 function prompt(req) {
 	output = JSON.stringify(req);
-	console.log(output);
+	// console.log(output);
 };
 
 
@@ -20,11 +20,13 @@ function Process(n, t, r) {
 
 		// Fetching missing requirements
 		for (req in this.requirements) {
-			if(!this.requirements[req].condition) {
+			output = "fini";
+			if(!this.requirements[req].condition()) {
 				this.requirements[req].method();
 				break;
 			}
 		};
+		console.log(output);
 	};
 }
 
@@ -47,9 +49,9 @@ function User(ln, fn, dob) {
 var thisUser = new User('Conges');
 var requirements =
 {
-	"first_name": {condition: thisUser.has("first_name"), method: function() { prompt({name: "first_name", type: "text", question: "What is your first name?", placeholder: "E.g. René"})}},
-	"date_of_birth": {condition: thisUser.has("date_of_birth"), method: function() { prompt({name: "date_of_birth", type: "text", question: "Please enter your birthdate", placeholder: "01/01/1900"})}},
-	"rabbit": {condition: thisUser.has("rabbit"), method: function() { prompt({name: "rabbit", type: "text", question: "Please enter your rabbit", placeholder: "rabbit's name"})}},
+	"first_name": {condition: function() { return thisUser.first_name }, method: function() { prompt({name: "first_name", type: "text", question: "What is your first name?", placeholder: "E.g. René"})}, promise: function(nv) {thisUser.first_name = nv} },
+	"date_of_birth": {condition: function() {return thisUser.date_of_birth }, method: function() { prompt({name: "date_of_birth", type: "text", question: "Please enter your birthdate", placeholder: "01/01/1900"})}, promise: function(nv) {thisUser.date_of_birth = nv} },
+	"rabbit": {condition: function() { return thisUser.rabbit }, method: function() { prompt({name: "rabbit", type: "text", question: "Please enter your rabbit", placeholder: "rabbit's name"})}, promise: function(nv) {thisUser.rabbit = nv} },
 };
 var inscriptionMaif = new Process("S'inscrire à la MAIF", "subscription", requirements);
 
@@ -79,7 +81,7 @@ io.sockets.on('connection', function (socket) {
 	socket.on('message', function (message) {
 		// DIRTY CODE
 		// Completing requirement (sauf qu'en fait ça devrait changer la valeur originale)
-		inscriptionMaif.requirements[message[0].name].condition = message[0].value;
+		inscriptionMaif.requirements[message[0].name].promise(message[0].value);
 		console.log(message[0].value);
 		
 		// Relaunching process
