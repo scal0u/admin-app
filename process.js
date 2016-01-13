9e10cfbf9c76d0875c6760b157219210b2075043
Pascal Conges <pascal.conges@brewers.co.uk>
2016-01-13 17:08:16 +0000

    Successful Firebase integration!

--- process.js
+++ process.js
@@ -25,13 +25,14 @@ function prompt(q) {
 };
 
 function Process(obj) {
-	this.steps = {};
-	this.promise = function() {};
-	if(obj) this = obj;
+	if(obj) {
+		this.name = obj.name;
+		this.type = obj.type;
+		this.steps = obj.steps;
+		this.promises = obj.promises;
+	} 
 
 	this.launch = function() {
-
-		console.log("Parent process: "+this.parent_process);
 
 		this.active = true;
 
@@ -58,7 +59,6 @@ function Process(obj) {
 			for(promise in this.promises) if(this.promises[promise].type == "user_update") thisUser[this.promises[promise].key] = this.promises[promise].value;
 			output = {dialog: "<h2>Thank you.</h2> Process <b>"+ this.name+"</b> is now complete"}; // Inform user
 			this.active = false; // Deactivate process
-			// this.promise(); // Running process' promise
 			if(this.parent_process) this.parent_process.launch();
 		}
 	};
@@ -84,49 +84,16 @@ processes = [];
 var Firebase = require("firebase");
 var myFirebaseRef = new Firebase("https://boiling-torch-1466.firebaseio.com/");
 
-myFirebaseRef.child("models/processes/vaccination").set({
-	name: "Vaccination",
-	type: "administrative",
-	steps: {
-		getName: {
-			condition: "thisUser.first_name",
-			method: "prompt",
-			prompt: {name: "first_name", type: "text", question: "What is your first name?", placeholder: "E.g. René"}
-		},
-	}
-});
-
-myFirebaseRef.child("models/processes/vaccination/type").on("value", function(snapshot) {
- 	console.log(snapshot.val());
+myFirebaseRef.child("models/processes").on("value", function(snapshot) {
+ 	var obj = snapshot.val();
+	global["getInsurance"] = new Process(obj.getInsurance);
+	global["vaccination"] = new Process(obj.vaccination);
+	global["voyageGuyane"] = new Process(obj.voyageGuyane);
+	// Launch
+	voyageGuyane.launch();
 });
 
 thisUser = new User('Congès');
-
-voyageGuyane = new Process();
-voyageGuyane.name = "Voyage en Guyane";
-voyageGuyane.type = "travel";
-voyageGuyane.steps.first_name = {condition: {type: "user_data", value: "first_name" }, method: {type: "prompt", value: {name: "first_name", type: "text", question: "What is your first name?", placeholder: "E.g. René"}}, promises: [ {type: "user_update", key: "first_name"} ] };
-voyageGuyane.steps.vaccinated = {condition: {type: "user_data", value: "vaccinated" }, method: {type: "switch_process", value: "vaccination"} };
-voyageGuyane.steps.insured = {condition: {type: "user_data", value: "insured" }, method: {type: "switch_process", value: "getInsurance"} };
-voyageGuyane.steps.date_of_birth = {condition: {type: "user_data", value: "date_of_birth" }, method: {type: "prompt", value: {name: "date_of_birth", type: "text", question: "Please enter your birthdate", placeholder: "01/01/1900"}}, promises: [ {type: "user_update", key: "date_of_birth"} ] };
-
-getInsurance = new Process();
-getInsurance.name = "S'inscrire à la MAIF";
-getInsurance.type = "subscription";
-getInsurance.steps.date_of_birth = {condition: {type: "user_data", value: "date_of_birth" }, method: {type: "prompt", value: {name: "date_of_birth", type: "text", question: "Please enter your birthdate", placeholder: "E.g. 01/01/1900"}}, promises: [ {type: "user_update", key: "date_of_birth"} ] };
-getInsurance.steps.ni_number = {condition: {type: "user_data", value: "ni_number" }, method: {type: "prompt", value: {name: "ni_number", type: "text", question: "Please enter your National Insurance Number", placeholder: "Your number"}}, promises: [ {type: "user_update", key: "ni_number"} ] };
-getInsurance.promises = [ {type: "user_update", key: "insured", value: true} ];
-
-vaccination = new Process();
-vaccination.name = "Vaccination";
-vaccination.type = "administrative";
-vaccination.steps.height = {condition: {type: "user_data", value: "height" }, method: {type: "prompt", value: {name: "height", type: "text", question: "Please enter your height", placeholder: "130cm"}}, promises: [ {type: "user_update", key: "height" } ] };
-vaccination.steps.weight = {condition: {type: "user_data", value: "weight" }, method: {type: "prompt", value: {name: "weight", type: "text", question: "Please enter your weight", placeholder: "25kg"}}, promises: [ {type: "user_update", key: "weight" } ] };
-vaccination.promises = [ {type: "user_update", key: "vaccinated", value: true} ];
-
-
-// Launch
-voyageGuyane.launch();
 
 
 ////////////
